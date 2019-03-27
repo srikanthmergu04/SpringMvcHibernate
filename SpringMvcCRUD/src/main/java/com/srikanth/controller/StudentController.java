3c1530eb76629251d6c59b8e38453363905641eb
TT124 <TT124@192.168.1.240>
2019-03-27 10:53:01 +0530

    2nd Commit - sessionFactory injected , db connection Successfull

--- SpringMvcCRUD/src/main/java/com/srikanth/controller/StudentController.java
+++ SpringMvcCRUD/src/main/java/com/srikanth/controller/StudentController.java
@@ -1,6 +1,8 @@
 package com.srikanth.controller;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +26,16 @@ public class StudentController {
 		StudentDetails student = new StudentDetails();
 		
 		student.setsName("Vinayak");
-		System.out.println("session obj = "+sessionFactory.openSession().save(student));
+		//student.setId(95);
+		System.out.println("session obj = "+sessionFactory.openSession());
+		Session session = null;
+		session = sessionFactory.openSession();
+		
+		Transaction trnx = session.beginTransaction();
+		
+		session.save(student);
+		
+		trnx.commit();
 		//m.addAttribute("student", new Student());
 		//System.out.println("JdbcTemplate = "+template);
 		//System.out.println("service in Student Controller = "+service);
